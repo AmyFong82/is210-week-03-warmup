d7cc801bb10e9eceaa76ce2b8ef73b60749aea50
Amy Fong <AmyhFong@gmail.com>
2016-09-10 01:17:51 +0000

    Submitting task_09 thru task_15.

--- task_09.py
+++ task_09.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""A computer that can build the question."""
+"""A computer that can build the question.
+
+This is a multi-line docstring.
+Line 1 of the description.
+Line 2 of the description.
+Line 3 of the description.
+"""
 
 THE_ANSWER_TO_EVERYTHING = 42
 
